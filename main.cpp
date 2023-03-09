688964a5d9fbe3a138ec3f59087bb3363b08a6d0
jenseki <jenseki24@gmail.com>
2023-03-09 13:37:42 -0800

    Brute force way

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@ Source: https://www.geeksforgeeks.org/number-guessing-game-in-cpp-using-rand-fun
 #include <cstdlib>
 #include <ctime>
 
+
+
 int main()
 {
     std::cout << "\n\t\t\tWelcome to the Guessing Game!" << std::endl;
@@ -51,24 +53,148 @@ int main()
                                 << playerChoice
                                 << " is the secret number" << std::endl;
                     std::cout << "\t\t\t Thanks for playing."
-                                << endl;
+                                << std::endl;
                     break;
                 }
                 else{
                     std::cout << "Nope, " << playerChoice
                                 << " is not the correct number\n";
+                    if(playerChoice > secretNumber){
+                        std::cout << "The secret number is "
+                                        "smaller than the number "
+                                        "you have chosen"
+                                    << std::endl;
+                    }
+                    else{
+                        std::cout << "The secret number is "
+                                        "greater than the number "
+                                        "you have chosen"
+                                    << std::endl;
+                    }
+                    choicesLeft--;
+                    std::cout << choicesLeft << " choices left. "
+                                << std::endl;
+                    if(choicesLeft == 0){
+                        std::cout << "You couldn't find the "
+                                        "secret number, it was "
+                                    << secretNumber
+                                    << ", You lose!\n\n";
+                        std::cout << "Play the game again to "
+                                        "win!\n\n";
+                    }
                 }
             }
         }
 
         // Medium difficulty
         else if(difficultyChoice == 2){
+            std::cout << "\nYou have 7 chances to find the "
+                        "secret number between 1 and 100.";
+            int choicesLeft = 7;
+            for(int i = 1; i <= 7; i++){
+                // prompt player
+                std::cout << "\n\nEnter the number: ";
+                std::cin >> playerChoice;
 
+                // compare playerChoice to secretNumber
+                if(playerChoice == secretNumber){
+                    std::cout << "Well played! YoU wOn! "
+                                << playerChoice
+                                << " is the secret number" << std::endl;
+                    std::cout << "\t\t\t Thanks for playing."
+                                << std::endl;
+                    break;
+                }
+                else{
+                    std::cout << "Nope, " << playerChoice
+                                << " is not the correct number\n";
+                    if(playerChoice > secretNumber){
+                        std::cout << "The secret number is "
+                                        "smaller than the number "
+                                        "you have chosen"
+                                    << std::endl;
+                    }
+                    else{
+                        std::cout << "The secret number is "
+                                        "greater than the number "
+                                        "you have chosen"
+                                    << std::endl;
+                    }
+                    choicesLeft--;
+                    std::cout << choicesLeft << " choices left. "
+                                << std::endl;
+                    if(choicesLeft == 0){
+                        std::cout << "You couldn't find the "
+                                        "secret number, it was "
+                                    << secretNumber
+                                    << ", You lose!\n\n";
+                        std::cout << "Play the game again to "
+                                        "win!\n\n";
+                    }
+                }
+            }
         }
 
         // Hard difficulty
         else if(difficultyChoice == 3){
+            std::cout << "\nYou have 5 chances to find the "
+                        "secret number between 1 and 100.";
+            int choicesLeft = 5;
+            for(int i = 1; i <= 5; i++){
+                // prompt player
+                std::cout << "\n\nEnter the number: ";
+                std::cin >> playerChoice;
 
+                // compare playerChoice to secretNumber
+                if(playerChoice == secretNumber){
+                    std::cout << "Well played! YoU wOn! "
+                                << playerChoice
+                                << " is the secret number" << std::endl;
+                    std::cout << "\t\t\t Thanks for playing."
+                                << std::endl;
+                    break;
+                }
+                else{
+                    std::cout << "Nope, " << playerChoice
+                                << " is not the correct number\n";
+                    if(playerChoice > secretNumber){
+                        std::cout << "The secret number is "
+                                        "smaller than the number "
+                                        "you have chosen"
+                                    << std::endl;
+                    }
+                    else{
+                        std::cout << "The secret number is "
+                                        "greater than the number "
+                                        "you have chosen"
+                                    << std::endl;
+                    }
+                    choicesLeft--;
+                    std::cout << choicesLeft << " choices left. "
+                                << std::endl;
+                    if(choicesLeft == 0){
+                        std::cout << "You couldn't find the "
+                                        "secret number, it was "
+                                    << secretNumber
+                                    << ", You lose!\n\n";
+                        std::cout << "Play the game again to "
+                                        "win!\n\n";
+                    }
+                }
+            }
+        }
+
+        // Exit game
+        else if(difficultyChoice == 0){
+            exit(0);
+        }
+
+        // Invalid choice
+        else{
+            std::cout << "Invalid choice, enter valid choice to "
+                "play the game! (0,1,2,3)"
+                << std::endl;
         }
     }
+    return 0;
 }
